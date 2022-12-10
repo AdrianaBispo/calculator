452a946b17632d6c0535493d6a927bab35f7ec6b
dev <adriana.bispo283@gmail.com>
2022-12-10 16:35:23 -0300

    classe my app

---
+++ lib/appwidget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:calculator/view/calculadorapage.dart';
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(
+        primaryColor: Colors.white, //
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
