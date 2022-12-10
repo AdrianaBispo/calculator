147346e1dd2a903027ee402d95abd70bae3fa0dd
dev <adriana.bispo283@gmail.com>
2022-12-10 16:35:35 -0300

    retirada a classe myapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:calculator/calculadora.dart';
+import 'appwidget.dart';
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculator',
-      theme: ThemeData(
-        primaryColor: Colors.white, //
-      ),
-      home: const HomePage(),
-    );
-  }
-}
 
